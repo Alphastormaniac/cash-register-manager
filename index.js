6ae8a856076e6b37e66301adaa5461907117a72b
Sai Pavan <sai.pavan7829@gmail.com>
2021-09-29 20:52:38 +0530

    added qs

---
+++ index.js
@@ -0,0 +1,16 @@
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+
+const errorDiv = document.querySelector(".error-message");
+
+const cashGivenDiv = document.querySelector(".cash-input");
+const changeReturnedDiv = document.querySelector(".change-returned");
+
+const output = document.querySelector("#output");
+
+const nextButton = document.querySelector("#next-button");
+const checkButton = document.querySelector("#check-button");
+
+const Notes = document.querySelectorAll(".notes");
+
+const notesArray = [2000, 500, 100, 20, 10, 5, 1];
